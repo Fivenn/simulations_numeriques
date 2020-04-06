63c0f53c164f26b18a2fc412e24bccedd9486b92
Nicolas SUEUR <nicolas1.sueur@orange.com>
2020-04-06 16:34:05 +0200

    develop exponentiation function

---
+++ exercice_1/exponentiation.py
@@ -0,0 +1,18 @@
+def exponentiation(x, n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return x
+    else:
+        tmp = exponentiation(x, n/2)
+        if n %2 == 0:
+            return tmp * tmp
+        else:
+            return x * tmp * tmp
+
+x = int(input('Nombre entier ou réel : '))
+n = int(input('Nombre entier positif : '))
+
+print(exponentiation(x, n))
+
+
