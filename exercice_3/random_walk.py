7d6d1dd3bdbdeacddde9c9bd0b44c5cd650c5c78
Nicolas SUEUR <nicolas1.sueur@orange.com>
2020-04-15 11:42:24 +0200

    add random walk

---
+++ exercice_3/random_walk.py
@@ -0,0 +1,25 @@
+import numpy
+import random
+from matplotlib import pyplot
+
+N_STEPS = 20
+prob = 0.5
+
+def RandomWalk(N,p,line):
+    position = numpy.empty(N)
+    position[0] = 0
+    pos_counter = 0
+    steps = numpy.arange(N)
+    for i in range(1,N):
+        test = random.random()
+        if test >= p:
+            pos_counter += 1
+        else:
+            pos_counter -= 1
+        position[i] = pos_counter
+    pyplot.plot(steps, position, line)
+    return None
+
+pyplot.figure()
+RandomWalk(N_STEPS, prob, line = 'o--')
+pyplot.show()
